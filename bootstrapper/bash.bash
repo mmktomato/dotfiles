bb130fca663e009619fa4976e96f9d1706036cf6
mmktomato <mkt.motohashi@gmail.com>
2018-08-14 16:03:19 +0900

    use bash

--- bootstrapper/bash.bash
+++ bootstrapper/bash.bash
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function bootstrap_bash() {
     local CLONE_DIR=dotfiles
--- bootstrapper/emacs.bash
+++ bootstrapper/emacs.bash
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function bootstrap_emacs() {
     local CLONE_DIR=dotfiles
